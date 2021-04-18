79e577cc4445b3ad820eb115e86784f98ccab4cd
JacksonCrawford <jacksoncrawford@protonmail.com>
2021-04-18 15:24:18 -0400

    Fix wallpaper path

--- awesome/themes/ethereal/theme.lua
+++ awesome/themes/ethereal/theme.lua
@@ -15,7 +15,7 @@ local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
 local theme                                     = {}
 theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/ethereal"
-theme.wallpaper                                 = "../../walls/fogRock.jpg"
+theme.wallpaper                                 = os.getenv("HOME") .. "/.config/awesome/walls/fogRock.jpg"
 theme.font                                      = "Icomoon 12"
 theme.taglist_font                              = "Microns 13"
 theme.sysFont					= "Futura 12"
